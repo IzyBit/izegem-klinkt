40833ceac58d5e517f87cf2a8797d317cb4a2491
Lukas Olivier <olivier.lukas2003@gmail.com>
2024-03-26 19:49:55 +0100

    fix: format error in nuxt config, fixed seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,19 @@ import { resolve } from 'path'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Izegem Klinkt',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Izegem Klinkt - Muzikale Tournée in de Pekkersstad. 4 mei 2024. Gratis toegang. 14 deelnemende cafés. 14 bands. 1 stad. 1 muzikale tournée.' },
+        { name: 'keywords', content: 'Izegem, Klinkt, Muziek, Pekkersstad, Tournée, Gratis, 14 cafés, 14 bands, 1 stad, 1 muzikale tournée, 4 mei 2024, kroegentocht' },
+        { name: 'author', content: 'Lukas Olivier' },
+        { name: 'canonical', content: 'https://www.izegemklinkt.be' },
+      ],
+    },
+  },
   modules: ['nuxt-primevue', "@nuxt/image"],
   alias: {
     "@gallery": "/assets/images/gallery/",
@@ -15,18 +28,6 @@ export default defineNuxtConfig({
     'primeicons/primeicons.css',
     '~/assets/css/main.css',
   ],
-  // SEO
-  head: {
-    title: 'Izegem Klinkt',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'description', content: 'Izegem Klinkt - Muzikale Tournée in de Pekkersstad. 4 mei 2024. Gratis toegang. 14 deelnemende cafés. 14 bands. 1 stad. 1 muzikale tournée.' },
-      { name: 'keywords', content: 'Izegem, Klinkt, Muziek, Pekkersstad, Tournée, Gratis, 14 cafés, 14 bands, 1 stad, 1 muzikale tournée, 4 mei 2024, kroegentocht' },
-      { name: 'author', content: 'Lukas Olivier' },
-      { name: 'canonical', content: 'https://www.izegemklinkt.be' },
-    ],
-  },
   postcss: {
     plugins: {
       tailwindcss: {},
